0cc7919b59518276a5d75a2d84ef18612c4ca8b3
Shabda Raaj <shabda@agiliq.com>
2012-07-24 13:28:09 +0530

    75% of the way in

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
