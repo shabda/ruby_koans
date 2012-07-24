914d36d3c14d7fe2487f4d1c46cbedb9465db8c7
Shabda Raaj <shabda@agiliq.com>
2012-07-24 10:58:28 +0530

    Finished traingle2. 159/280

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if a==0 || b== 0 || c ==0
+    raise TriangleError, "0"
+  end
+  if a<0 || b<0 || c<0
+    raise TriangleError, "neg"
+  end
+  if a+b<=c || b+c <= a || c+a <= b
+    raise TriangleError, "no triangle possible"
+  end
+  
   if a==b && b==c && c==a
     :equilateral
   elsif a==b || b ==c || c == a
